0eba9ea4b213d534e5916938fb6f7667a3cbe0e2
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-03-11 09:44:19 +0100

    added v2

--- run.py
+++ run.py
@@ -1,34 +1,49 @@
 import requests
-from flask import Flask
+from flask import Flask, jsonify
+from statistics import mean
+import time
+from threading import Thread
 
 app = Flask('')
 
+avgvalues = [0, 0, 3, 1, 5]
+
+cached_value = 0
+mathed_value = 0
+
+def count_update(avgvalues):
+    global mathed_value
+    while True:
+        mathed_value = mathed_value + int(round(mean(avgvalues)))
+        time.sleep(900)
+
+def cache_update():
+    global cached_value, mathed_value
+    while True:
+        url = "https://api.spiget.org/v2/resources/63714"
+        r = requests.get(url).json()
+        if not cached_value == r['downloads']:
+            cached_value = r['downloads']
+            mathed_value = r['downloads']
+        time.sleep(1800)
+
+
+@app.route('/v2/bedwars/')
+def bedwarsv2():
+    global cached_value
+    url = "https://api.spiget.org/v2/resources/63714"
+    r = requests.get(url).json()
+    x = {"approximate": mathed_value, "real": r['downloads']}
+    return jsonify(x)
 
 @app.route('/bedwars')
 def bedwars():
-    # url = "https://api.spiget.org/v2/resources/63714"
-    url = "https://api.spigotmc.org/simple/0.1/index.php?action=getResource&id=63714"
-    r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; '
-                                                 'Intel Mac OS X 10_12_6) AppleWebKit/53'
-                                                 '7.36(KHTML, like Gecko) Chrome/63.0.32'
-                                                 '39.132 Safari/537.36'}).json()
-    return "<p>" + str(r['stats']['downloads']) + "</p>"
+    url = "https://api.spiget.org/v2/resources/63714"
+    r = requests.get(url).json()
+    return "<p>" + str(r['downloads']) + "</p>"
 
-
-@app.route('/sbahypixelify')
-def hypixelify():
-    # url = "https://api.spiget.org/v2/resources/79505"
-    url = "https://api.spigotmc.org/simple/0.1/index.php?action=getResource&id=79505"
-    r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; '
-                                                 'Intel Mac OS X 10_12_6) AppleWebKit/53'
-                                                 '7.36(KHTML, like Gecko) Chrome/63.0.32'
-                                                 '39.132 Safari/537.36'}).json()
-    return "<p>" + str(r['stats']['downloads']) + "</p>"
-
-
-@app.route('/')
-def main():
-    return "ScreamingBedWars: " + bedwars() + "SBAHypixelify: " + hypixelify()
-
-
+url = "https://api.spiget.org/v2/resources/63714"
+r = requests.get(url).json()
+Thread(target=cache_update).start()
+Thread(target=lambda: count_update(avgvalues)).start()
 app.run(host="0.0.0.0", port=8080)
