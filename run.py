97563a04535721f797f63d9e4f18ad7c9ad2f9b2
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-03-11 19:15:32 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,33 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver.v2 as uc
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
-from time import sleep
-
-def get(driver, url):
-    try:
-        driver.get(url)
-    finally:
-        sleep(4)
 
 app = Flask('')
 
-chrome_options = Options()
+chrome_options = uc.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-chrome_options.add_experimental_option('useAutomationExtension', False)
-chrome_options.add_argument("--disable-blink-features")
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+driver = uc.Chrome(options=chrome_options)
 
 @app.route('/v3/bedwars/')
 def bedwarsv3():
     global driver
     url = "https://www.spigotmc.org/resources/screaming-bedwars-1-9-1-16.63714/"
-    get(driver, url)
+    driver.get_in(url)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     data = soup.find("dl", {"class": "downloadCount"}).find("dd").text.replace(",", "")
     return jsonify({"result": data})
